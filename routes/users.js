00bc185162befd3e18b44092e6a60d7bc538cc60
horaciokar <horaciokar@gmail.com>
2026-05-23 20:32:47 -0500

    Primer versión del MVP StickerMatch

---
+++ routes/users.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+
+const pool = require('../db');
+
+router.post('/', async (req, res) => {
+
+    const { username } = req.body;
+
+    try {
+
+        const result = await pool.query(
+            'INSERT INTO users (username) VALUES ($1) RETURNING *',
+            [username]
+        );
+
+        res.json(result.rows[0]);
+
+    } catch (error) {
+
+        console.error(error);
+        res.status(500).json({
+            error: 'Error creando usuario'
+        });
+
+    }
+
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+
+const usersRoutes = require('./routes/users');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/users', usersRoutes);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('StickerMatch funcionando');
+});
+
+app.listen(3000, () => {
+    console.log('Servidor iniciado en puerto 3000');
+});
